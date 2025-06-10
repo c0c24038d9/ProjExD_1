f5a83c591f105d80d612607ddd3bd55e9e2bc556
c0c24038d9 <c0c24038d9@edu.teu.ac.jp>
2025-06-10 16:14:48 +0900

    こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,29 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_rct = kk_img.get_rect() 
+    kk_rct.center = 300, 200 #こうかとんの中心座標
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-            
+        key_lst = pg.key.get_pressed() #キーの押下状態を取得
+        # print(key_lst)
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1) #↑キーが押されたら上に移動
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
+
         tmr = tmr%3200
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2, [-tmr+1600, 0])
         screen.blit(bg_img, [-tmr+3200, 0])
-        screen.blit(kk_img, [300, 200])
+        # screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
