b9e73dd3c76f4449a0491d2c1b81a00404382d06
c0c24038d9 <c0c24038d9@edu.teu.ac.jp>
2025-06-10 15:43:54 +0900

    練習７：背景画像2枚目

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
