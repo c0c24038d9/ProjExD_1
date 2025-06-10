1ba4e8aa33640c3534d010725c72f2af5029e024
c0c24038d9 <c0c24038d9@edu.teu.ac.jp>
2025-06-10 16:05:24 +0900

    練習9:背景画像ループ

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2=pg.transform.flip(bg_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
@@ -18,9 +18,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+            
+        tmr = tmr%3200
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2, [-tmr+1600, 0])
+        screen.blit(bg_img, [-tmr+3200, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
