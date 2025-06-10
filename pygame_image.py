733aa2bdb214bdbdd77058c495f29e1ef10b8935
c0c24038d9 <c0c24038d9@edu.teu.ac.jp>
2025-06-10 15:29:36 +0900

    こうかとん読込

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
